12ffc17f05e5cb2e1b8bcc5e02761d8dc505b0c1
Josue <josueg4rcia@gmail.com>
2025-10-01 02:52:17 -0400

    Registro y login

--- pyschool-backend/index.js
+++ pyschool-backend/index.js
@@ -1,14 +1,36 @@
 import express from 'express'
 import { PORT } from './config.js'
+import { UserRepository } from './userRepository.js'
 
 const app = express()
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send(`Hello PySon!`)
 })
 
-app.post('/login', (req, res) => {})
-app.post('/register', (req, res) => {})
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body
+  try {
+    const user = await UserRepository.login({ username, password})
+    res.send({ user })
+  } catch (error) {
+    res.status(401).send(error.message)
+  }
+})
+
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body 
+  console.log(req.body)
+
+  try {
+    const id = await UserRepository.create({ username, password })
+    res.send({ id })
+  } catch (error){
+    res.status(400).send(error.message)
+  }
+})
+
 app.post('/logout', (req, res) => {})
 app.get('/protected', (req, res) => {})
 
