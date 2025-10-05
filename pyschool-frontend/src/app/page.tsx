334892e5d2d304983d2ca9ce26874770808b78b6
Milena <miletorrico0041@gmail.com>
2025-10-05 13:23:15 -0400

    Iconos login

--- pyschool-frontend/src/app/page.tsx
+++ pyschool-frontend/src/app/page.tsx
@@ -1,7 +1,13 @@
 import Image from "next/image";
+import { MdAlternateEmail } from 'react-icons/md'; // @ símbolo
+import { FiLock } from 'react-icons/fi'; // Candado
+import InputIcon from "@/ui/components/atoms/icons";
 
 export default function Home() {
   return (
-    <div>prueba</div>    
+    <div>
+      <InputIcon icon={MdAlternateEmail} />
+      <InputIcon icon={FiLock} />
+    </div>
   );
 }
