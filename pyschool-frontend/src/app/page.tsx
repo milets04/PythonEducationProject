2afc9587991c669f12a0a3380c6e75fc27d8ac76
floramos06 <ramosg_flor@outlook.com>
2025-10-07 15:26:31 -0400

    Creaccione y correciones de headerLandingpage con prueba en page

--- pyschool-frontend/src/app/page.tsx
+++ pyschool-frontend/src/app/page.tsx
@@ -2,11 +2,13 @@
 import Image from "next/image";
 /*import BtnHeader from "@/ui/components/atoms/btnHeader";
 import CoverText from "@/ui/components/atoms/coverText";
-import CoverDescriptionText from "@/ui/components/atoms/coverDescriptionText";*/
+import CoverDescriptionText from "@/ui/components/atoms/coverDescriptionText";
 import Img from "@/ui/components/atoms/img";
+import HeaderLandingpage from "@/ui/components/molecules/headerLandinpage";*/
 export default function Home() {
   return (
-    <div>prueba
+    <div>
+      prueba
     </div>    
   );
 }
