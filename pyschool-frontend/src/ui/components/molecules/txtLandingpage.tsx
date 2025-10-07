8d2fb97961e2fdb6d50424afc458227aa25fe638
floramos06 <ramosg_flor@outlook.com>
2025-10-07 16:49:31 -0400

    Ajuste de texto y salto de linea

--- pyschool-frontend/src/ui/components/molecules/txtLandingpage.tsx
+++ pyschool-frontend/src/ui/components/molecules/txtLandingpage.tsx
@@ -13,21 +13,21 @@ type TxtLandingpageProps = {
 };
 
 const TxtLandingpage: React.FC<TxtLandingpageProps> = ({
-  title = "Let's learn python with us",
-  description = "A good software developer works with discipline and perseverance from the first day",
+  title = "Let's learn python<br />with us",
+  description = "A good software developer works with discipline and<br /> perseverance from the first day",
   imageSrc = "/images/Landingpage.png",
   imageAlt = "Python learning illustration",
 
 }) => {
   return (
     <section
-      className={`w-full flex flex-col-reverse md:flex-row items-center justify-between gap-8 px-8 py-8 bg-transparent`}
+      className={`w-full flex flex-col-reverse md:flex-row items-center justify-center gap-8 px-8 py-8 bg-transparent`}
     >
-      <div className="flex flex-col items-center md:items-start text-center md:text-left max-w-lg">
-        <CoverText text={title} size="large"/>
-        <CoverDescriptionText text={description} size="small" />
+      <div className="flex flex-col items-center text-center max-w-lg">
+        <CoverText text={title} size="large" />
+        <CoverDescriptionText text={description} size="medium" />
       </div>
-      <div className="flex justify-center md:justify-end w-full md:w-auto">
+      <div className="flex justify-center w-full md:w-auto">
         <Img src={imageSrc} alt={imageAlt} width={420} height={300} />
       </div>
     </section>
