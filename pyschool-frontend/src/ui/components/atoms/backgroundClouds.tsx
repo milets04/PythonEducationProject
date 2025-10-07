51ca37de07cbb47604363ec644aabf6c062dcc0f
floramos06 <ramosg_flor@outlook.com>
2025-10-07 19:20:51 -0400

    feat(frontend): Landingpage funcionando en page

--- pyschool-frontend/src/ui/components/atoms/backgroundClouds.tsx
+++ pyschool-frontend/src/ui/components/atoms/backgroundClouds.tsx
@@ -17,7 +17,6 @@ const BackgroundClouds: React.FC = () => (
         maskPosition: "center",
       }}
     />
-
     <div
       className="absolute bottom-6 left-[-20px] w-[360px] h-[200px] opacity-100"
       style={{
@@ -32,7 +31,6 @@ const BackgroundClouds: React.FC = () => (
         maskPosition: "center",
       }}
     />
-
     <div
       className="absolute top-[80px] right-[-10px] w-[320px] h-[180px] opacity-100"
       style={{
