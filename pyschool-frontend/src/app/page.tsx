653b434554f03d2503478b52fc27e43f38d63144
Milena <miletorrico0041@gmail.com>
2025-10-07 17:28:44 -0400

    molecula iconInputForm terminado

--- pyschool-frontend/src/app/page.tsx
+++ pyschool-frontend/src/app/page.tsx
@@ -1,11 +1,33 @@
 "use client";
 import Image from "next/image";
+import { MdAlternateEmail } from 'react-icons/md';
+import { FiLock } from 'react-icons/fi';
+import InputWithIcon from '@/ui/components/molecules/iconInputForm';
 import Subtitle from "@/ui/components/atoms/subtLog";
 
+import React, { useState } from "react";
+
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div>
       <Subtitle text="Email" />
+      <InputWithIcon
+        icon={MdAlternateEmail}
+        value={email}
+        placeholder="Enter your Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <Subtitle text="Password" />
+      <InputWithIcon
+        icon={FiLock}
+        value={password}
+        placeholder="Enter your Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
     </div>
   );
 }
---
+++ pyschool-frontend/src/ui/components/molecules/iconInputForm.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { IconType } from 'react-icons';
+import Input from '@/ui/components/atoms/input';
+import InputIcon from '@/ui/components/atoms/icons';
+
+interface InputWithIconProps {
+  id?: string;
+  value: string;
+  label?: string;
+  placeholder?: string;
+  icon: IconType;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  className?: string;
+}
+
+const InputWithIcon: React.FC<InputWithIconProps> = ({
+  id = 'input-id',
+  value,
+  label = '',
+  placeholder = '',
+  icon,
+  onChange,
+  className = '',
+}) => {
+  return (
+    <div className={`w-390px ${className}`}>
+      {label && (
+        <label htmlFor={id} className="block text-sm font-tLog text-tLog mb-2">
+          {label}
+        </label>
+      )}
+      <div className="relative w-390px">
+        <div className="absolute left-3 top-1/2 transform -translate-y-1/2">
+          <InputIcon icon={icon} />
+        </div>
+        <input
+          id={id}
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+          className={`w-390px border border-gray-300 rounded-md pl-12 pr-3 py-2 focus:outline-none focus:ring-1 focus:ring-gray-300`}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default InputWithIcon;
