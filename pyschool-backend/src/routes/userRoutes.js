0c5e0cb8bb0ff033a18d12ef8a5e3194f997bf9c
Josue <josueg4rcia@gmail.com>
2025-10-08 01:53:44 -0400

    Autenticacion con validaciones para usuarios genericos

--- pyschool-backend/src/routes/userRoutes.js
+++ pyschool-backend/src/routes/userRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import { createUser, deleteUser, getAllUser, getUserById, updateUser } from '../controllers/userController.js'
+import validateUser from '../middlewares/inputVUserValidator.js'
 
 const router = express.Router()
 
 router.get('/user', getAllUser)
 router.get('/user/:id', getUserById)
-router.post('/user/', createUser)
+router.post('/user/', validateUser, createUser)
 router.delete('/user/:id', deleteUser)
-router.put('/user/:id', updateUser)
+router.put('/user/:id',validateUser, updateUser)
 
 export default router
