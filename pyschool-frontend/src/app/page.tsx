cfe63c3ec10cf8e96c4404cbb2700f79440b7d62
montmelina917 <montmelina917@gmail.com>
2025-10-07 20:40:17 -0400

    feat(frontend): arreglos del merge al main

--- pyschool-frontend/src/app/page.tsx
+++ pyschool-frontend/src/app/page.tsx
@@ -1,23 +1,13 @@
 "use client";
-<<<<<<< HEAD
-import Image from "next/image";
-import LandingpageTemplate from "@/ui/components/templates/landingpageTemplate";
-=======
 import LoginPage from "@/ui/components/templates/formSignPage";
 import Image from "next/image";
 import React, { useState } from "react";
->>>>>>> MolecAtMile
 
 export default function Page() {
   return (
     <div>
-<<<<<<< HEAD
-      <LandingpageTemplate />
-    </div>    
-=======
       <LoginPage />
     </div>
->>>>>>> MolecAtMile
   );
 }
 /*prueba lp*/
