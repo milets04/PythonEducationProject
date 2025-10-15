b3b174b11157e68dd05e8f360cb7080b8c6e2acb
Milena <miletorrico0041@gmail.com>
2025-10-15 10:57:01 -0400

    Correciones de formRegister

--- pyschool-frontend/src/ui/components/templates/formRegisterPage.tsx
+++ pyschool-frontend/src/ui/components/templates/formRegisterPage.tsx
@@ -1,27 +1,21 @@
-'use client';
-
 import Image from 'next/image';
 import RegisterForm from "@/ui/components/organisms/formRegister";
-import { useRouter } from 'next/navigation';
 
 const RegisterPage: React.FC = () => {
-  const router = useRouter();
-
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-100 to-blue-50 flex flex-col items-center justify-center relative overflow-hidden">
-      
-      <RegisterForm/>
-      
-      <button 
-        className="fixed top-5 left-5 z-[100] cursor-pointer bg-red-500 text-white px-4 py-2"
-        onClick={() => {
-          alert('CLICKED!!!');
-          router.push('/');
-        }}
-        type="button"
-      >
-        HOME
-      </button>
+      <div className="absolute top-5 left-5 flex items-center gap-2">
+        <Image
+          src="/images/pysonlogo.png"
+          alt="Decoration"
+          width={200}
+          height={200}
+          priority
+        />
+      </div>
+      <div>
+        <RegisterForm/>
+      </div>
     </div>
   );
 };
