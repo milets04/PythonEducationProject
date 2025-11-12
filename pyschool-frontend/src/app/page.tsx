2c475b3a0ffe05e3bb6670d74054b3613895d172
Milena <miletorrico0041@gmail.com>
2025-11-12 11:02:53 -0400

    LandingPage en page nuevamente

--- pyschool-frontend/src/app/page.tsx
+++ pyschool-frontend/src/app/page.tsx
@@ -1,16 +1,12 @@
 "use client";
 
 import React from "react";
-import Logs, { TopicVersion } from "@/ui/components/organisms/logs";
+import LandingpageTemplate from "@/ui/components/templates/landingpageTemplate";
 
-const topicHistory: TopicVersion[] = [
-  { id: 31, createdAt: "2025-11-10 21:35:16.496" },
-  { id: 25, createdAt: "2025-11-10 21:06:21.690" }, 
-];
 export default function Page() {
   return (
-    <div>
-      <Logs versions={topicHistory} />
+    <div className="min-h-screen bg-[#C9DDDC] flex items-center justify-center">
+      <LandingpageTemplate />
     </div>
   );
 }
